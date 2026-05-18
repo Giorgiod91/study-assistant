dacb637a5be5cddf40890163134b45928da77c17
Giorgio Dettmar <giorgio.dettmar@gmx.de>
2026-05-18 15:40:46 +0200

    adding my own progressTracker in python

---
+++ backend/trackProgress.py
@@ -0,0 +1,14 @@
+
+# Creating method and more to track the progress and to calc progress and to define which
+# areas and more a givin progress points 
+
+# progress
+progress = 100
+
+# get Progress
+
+def getProgress():
+   return progress
+
+
+# 
