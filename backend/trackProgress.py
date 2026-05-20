323156dbe294cbbc5a0530ceb503913c989bca8f
Giorgio Dettmar <giorgio.dettmar@gmx.de>
2026-05-20 21:02:34 +0200

    adding some methods for the progress tracking part

--- backend/trackProgress.py
+++ backend/trackProgress.py
@@ -11,4 +11,22 @@ def getProgress():
    return progress
 
 
-# 
+# handle value of progress
+
+def progressValue():
+    done_task = 40
+    done_part_of_task = 10
+    set_date_for_task =5
+
+    return done_task, done_part_of_task
+
+
+# add value to progress
+
+def addValue(value):
+    if progress - value < 100:
+        progress += value
+
+        return progress
+
+
